5cbabb8d361ec775230be350b26e70c96ed7f4e8
Jay Shah <jays2249@gmail.com>
2024-10-06 19:05:05 +0530

    Added environment

--- src/app/core/app.module.ts
+++ src/app/core/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -7,27 +7,33 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from '../core/app-routing.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { SharedModule } from '../shared/shared.module';
+import { CoreModule } from './core.module';
 import { environment } from '../environments/environment';
+
+// import { API_URL } from './core/services/api-url.token';
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-    enabled: environment.production,
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
+      enabled:environment.production,
+    }),
+    provideHttpClient(withInterceptorsFromDi()), // New provider
+    CoreModule, // Optional: Contains interceptors, services, etc.
+    SharedModule,
+  ],
   providers: [
-    
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true } 
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    {provide: API_URL, useValue: environment.apiUrl}
   ],
   bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+// src/environments/environment.prod.ts
+
+export const environment = {
+    production: true,
+    apiUrl: 'https://api.yourdomain.com',
+    // Add other production-specific variables here
+  };
+  
